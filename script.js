3fa357994937d7ef2392f22a7cb2d2e13c903e16
Karoly Krisztian Kozak-Szilagyi <kozak.karoly.krisztian@gmail.com>
2026-05-08 10:10:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-        const btn = document.querySelector(".projekt-btn");
-        const loader = document.getElementById("loading-screen");
+    const btn = document.querySelector(".projekt-btn");
+    const loader = document.getElementById("loading-screen");
 
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
 
-            loader.style.display = "flex";
+        loader.style.display = "flex";
 
-            const target = btn.getAttribute("href");
+        const target = btn.getAttribute("href");
 
-            setTimeout(() => {
-                window.location.href = target;
-            }, 4000);
-        });
+        setTimeout(() => {
+            window.location.href = target;
+        }, 4500);
+    });
+});
+
+document.querySelector(".ms-defender").addEventListener("click", function() {
+    var length = document.querySelectorAll(".floating-virus").length;
+    document.querySelector(".adware").removeAttribute("src");
+    document.querySelector(".install-defender p").innerHTML = "Microsoft Defender erfolgreich installiert";
+    document.querySelector(".install-defender p").style.color = "green";
+    for (var i = 0; i < length; i++) {
+        document.querySelectorAll(".floating-virus")[i].removeAttribute("src");
+    }
 });
